7b5e58c82d25f4c4ba70e35862ed9bf227820e22
bmehder <bmehder@gmail.com>
2022-11-25 17:44:38 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Sveltetube from '$lib/Sveltetube.svelte'
+</script>
+
+<h1>Tomfoolery components</h1>
+
+<div>
+  <Sveltetube
+    id="XSK1Bt9O0Pw"
+    title="Sondre Lerche - Alone In The Night (feat. AURORA)"
+    color="rgba(255,255,255,0.85)"
+    radius="1rem"
+    shadow="0 10px 20px rgba(0,0,0,0.24)"
+  />
+</div>
+
+<style>
+  div {
+    max-width: 600px;
+    margin-block: 2rem;
+    margin-inline: auto;
+  }
+</style>
